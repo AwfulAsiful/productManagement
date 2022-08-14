6d41f144038cb48fa84e6d982087c69f7f64d0d1
AwfulAsiful <awful872@gmail.com>
2022-08-14 11:57:51 +0600

    Minor Changes

--- C213081.cpp
+++ C213081.cpp
@@ -2,11 +2,14 @@
 #include <fstream>
 bool check_prime(int n)
 {
+    if (n <= 1)
+        return false;
     for (int i = 2; i * i <= n; i++)
     {
         if (n % i == 0)
             return false;
     }
+
     return true;
 }
 using namespace std;
@@ -16,18 +19,18 @@ int main()
     ifstream input("input.txt");
     ofstream output("output.txt");
     int i = 1;
-    
+
     while (input >> arr[i])
     {
 
         if (check_prime(arr[i]))
         {
-            output << "The number " << arr[i] << " is prime"<<endl;
+            output << "The number " << arr[i] << " is prime" << endl;
             continue;
         }
         else
         {
-            output << "The number " << arr[i] << " is composite"<<endl;
+            output << "The number " << arr[i] << " is composite" << endl;
             continue;
         }
         i++;
