104d7011d6307198a2f3a444e30569636c720544
AwfulAsiful <awful872@gmail.com>
2022-08-21 21:47:55 +0600

    Assignment Done!

--- C213081.cpp
+++ C213081.cpp
@@ -1,38 +1,80 @@
 #include <iostream>
 #include <fstream>
-bool check_prime(int n)
+
+using namespace std;
+class Product
 {
-    if (n <= 1)
-        return false;
-    for (int i = 2; i * i <= n; i++)
-    {
-        if (n % i == 0)
-            return false;
+public:
+    int product_id;
+    string product_category;
+    string product_name;
+
+    void get_product(){
+        ifstream in("productIn.txt");
+        in>>product_id;
+        in.ignore();
+        getline(in,product_category);
+        
+        getline(in,product_name);
+
+        
+        in.close();
+    }
+    void set_product(){
+        ofstream out("productOut.txt");
+        out<<"Product ID: "<<product_id<<endl;
+        out<<"Product Category: "<<product_category<<endl;
+        out<<"Product Name: "<<product_name<<endl;
     }
 
-    return true;
-}
-using namespace std;
+};
+class soldProduct:public Product{
+   public:
+    int customer_id;
+    string price;
+    void get_soldProduct(){
+        ifstream in("customerIn.txt");
+        in>>customer_id>>price;
+        in.close();
+    }
+    void set_soldProduct(){
+        ofstream out("customerOut.txt");
+        out<<"Customer ID: "<<customer_id<<endl;
+        out<<"Price: "<<price;
+    }
+};
+class boughtProduct:public Product{
+    public:
+    int buyer_id;
+    string price;
+    string buyer_name;
+    void get_boughtProduct(){
+        ifstream in("buyerIn.txt");
+        in>>buyer_id>>price;
+        in.ignore();
+        getline(in,buyer_name);
+        in.close();
+    }
+    void set_boughtProduct(){
+        ofstream out("buyerOut.txt");
+        out<<"Buyer ID: "<<buyer_id<<endl;
+        out<<"Price: "<<price<<endl;
+        out<<"Buyer Name: "<<buyer_name<<endl;
+    }
+};
 int main()
 {
-    int arr[101];
-    ifstream input("input.txt");
-    ofstream output("output.txt");
-    int i = 1;
+    Product obj1;
+    obj1.get_product();
+    obj1.set_product();
 
-    while (input >> arr[i])
-    {
 
-        if (check_prime(arr[i]))
-        {
-            output << "The number " << arr[i] << " is prime" << endl;
-            continue;
-        }
-        else
-        {
-            output << "The number " << arr[i] << " is composite" << endl;
-            continue;
-        }
-        i++;
-    }
+    soldProduct obj2;
+    obj2.get_soldProduct();
+    obj2.set_soldProduct();
+
+
+    boughtProduct obj3;
+    obj3.get_boughtProduct();
+    obj3.set_boughtProduct();
 }
